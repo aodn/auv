c6e244478766c53d260c2408f12f5316bf4a4c01
tfotak <tfotak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-03-06 22:43:41 +0000

    Removed datasource
    The AUV viewer app does not need a datasource so removed configuration entirely, as a consequence the models,
    their controllers and all related tests were also removed.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -4,39 +4,7 @@
 //  the terms of the GNU General Public License
 //*************************************************//
 
-
-dataSource {
-    pooled = true
-}
-hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
-}
 // environment specific settings
 environments {
-
-    development {
-        dataSource {
-            //dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-	        driverClassName = "org.postgresql.Driver"
-	        dialect = org.hibernate.dialect.PostgreSQLDialect
-            url = "jdbc:postgresql://localhost:5432/auvusers"
-            username = "auv"
-            password = "auv"
-        }
-    }
-    test {
-        dataSource {
-	        dbCreate = "create-drop"
-	        url = "jdbc:hsqldb:mem:testDb"
-        }
-    }
-   production {
-        dataSource {
-            //dbCreate = "update" // one of 'create', 'create-drop','update'
-            jndiName = "java:comp/env/jdbc/auvusers"
-        }
-    }
 
 }
