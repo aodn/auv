d2dc9b315b42f6c495df4338c595a1457b36e1c2
Alex McKeown <a.mckeown@utas.edu.au>
2019-10-10 11:50:41 +1100

    Update bumpversion.sh

--- bumpversion.sh
+++ bumpversion.sh
@@ -2,18 +2,20 @@
 
 set -eux
 
-RELEASE_BRANCH=remotes/origin/angus_test
+RELEASE_BRANCH=angus_test
 
 bumpversion_build() {
   bump2version patch
 }
 
 bumpversion_release() {
+  git checkout origin/angus_test
   bump2version patch
   git config user.name "aodn-ci-build"
   git config user.email "a.mckeown@utas.edu.au"
   git config --list
   VERSION=$(bump2version --list --commit --allow-dirty release | grep -oP '^new_version=\K.*$')
+  
   git tag $VERSION
   git push origin --tags
 
